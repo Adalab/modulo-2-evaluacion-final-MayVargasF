6a60ca8c3ccffe72da3b71a2853a7f09ee78cb3c
MayVargasF <maytevargasfelix@gmail.com>
2022-07-16 13:04:29 +0200

    fetch done

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,9 @@ const listAnime = document.querySelector(".js_list_anime");
 
 let animes = [];
 let favorites = [];
+
+fetch("https://api.jikan.moe/v4/anime")
+  .then((response) => response.json())
+  .then((info) => {
+    animes = info.data;
+  });
