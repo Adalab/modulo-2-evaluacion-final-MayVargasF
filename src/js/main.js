9ad265b9bdab7b0787e11b299463769cdfd5a4ce
MayVargasF <maytevargasfelix@gmail.com>
2022-07-16 13:24:42 +0200

    Anime data painted on html

--- src/js/main.js
+++ src/js/main.js
@@ -9,4 +9,17 @@ fetch("https://api.jikan.moe/v4/anime")
   .then((response) => response.json())
   .then((info) => {
     animes = info.data;
+    console.log(animes);
+
+    let html = "";
+
+    for (const eachAnime of animes) {
+      html += `<li>`;
+      html += `<div class="animeContainer">`;
+      html += `<h3>${eachAnime.title}</h3>`;
+      html += `<img src=${eachAnime.images.jpg.image_url} alt="${eachAnime.title} cover"/>`;
+      html += `</div></li>`;
+    }
+
+    listAnime.innerHTML = html;
   });
