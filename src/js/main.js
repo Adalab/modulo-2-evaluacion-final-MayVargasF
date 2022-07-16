5251324289fc0d585ce8b554bb3e67dee8663d1e
MayVargasF <maytevargasfelix@gmail.com>
2022-07-16 13:33:37 +0200

    image placeholder added

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,17 @@ const listAnime = document.querySelector(".js_list_anime");
 let animes = [];
 let favorites = [];
 
+function imagePlaceholder(element) {
+  if (
+    element.images.jpg.image_url ===
+    "https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png"
+  ) {
+    ("https://via.placeholder.com/210x295/ffffff/666666/?text=TV");
+  } else {
+    return element.images.jpg.image_url;
+  }
+}
+
 fetch("https://api.jikan.moe/v4/anime")
   .then((response) => response.json())
   .then((info) => {
@@ -17,7 +28,9 @@ fetch("https://api.jikan.moe/v4/anime")
       html += `<li>`;
       html += `<div class="animeContainer">`;
       html += `<h3>${eachAnime.title}</h3>`;
-      html += `<img src=${eachAnime.images.jpg.image_url} alt="${eachAnime.title} cover"/>`;
+      html += `<img src=${imagePlaceholder(eachAnime)} alt="${
+        eachAnime.title
+      } cover"/>`;
       html += `</div></li>`;
     }
 
